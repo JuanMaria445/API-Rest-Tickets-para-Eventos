6e0537130364f20f78a4f5e1c73b76cb7d97e630
JuanMaria445 <69771376+JuanMaria445@users.noreply.github.com>
2023-07-06 04:30:21 -0300

    Add files via upload

---
+++ src/middlewares/passport.ts
@@ -0,0 +1,22 @@
+import { User } from "../entity/Users";
+import { Strategy, ExtractJwt, StrategyOptions } from "passport-jwt";
+
+const jwtSecret = process.env.JWT_SECRET;
+const opts: StrategyOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), //se va a extraer el JWT en el header
+  secretOrKey: jwtSecret
+};
+
+export default new Strategy(opts, async (payload, done) => { //se define la estrategia despues que se inicializa
+  try {
+    const user = await User.findOneBy({ id: parseInt(payload.id) }); //payload es lo que llega del ID TOKEN y se busca si existe ese usuario en la BD
+
+    if (user) {
+      return done(null, user); //si lo encuentra devuelve el usuario completo
+    }
+    return done(null, false); //si no lo encuentra devuelve false
+    //done es una funcion propia de passport.
+  } catch (error) {
+    console.log(error);
+  }
+});
---
+++ src/routes/users.router.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import {getUsers, createUser, updateUser, deleteUser} from "../controllers/user.controller";
+import {signIn, signUp, protectedEndpoint, refresh } from '../controllers/user.controller'
+import passport from 'passport'
+
+import authenticateToken from "../middlewares/authenticateToken";
+
+const router = Router();
+
+router.get("/users", authenticateToken,getUsers);
+
+router.post("/users", authenticateToken,createUser);
+router.put("/users/:id", authenticateToken,updateUser);
+router.delete("/users/:id", authenticateToken,deleteUser);
+
+
+//Agregar para jwt
+router.post('/signup', signUp); //usamos bcrypt para hashear la clave
+router.post('/signin', signIn); //comparamos usando bcrypt para compare desde el login el password mandado es el mismo de nuestra BD para poder enviarsela al cliente si esta todo bien
+router.post('/token', refresh); //genera un nuevo token válido, cuando se vence nos manda el token y le devolvemos uno nuevo.
+router.post('/protected', passport.authenticate('jwt', { session: false }), protectedEndpoint); //Me devuelve un correcto o ok.
+
+export default router;
